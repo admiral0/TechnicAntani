4b99f3f31a716dcfb60fca9f6a192b4577a28803
Radu Andries <admiral0@tuxfamily.org>
2015-06-15 23:36:53 +0200

    WIP

--- sshserver/src/main/kotlin/net/adamcin/httpsig/jce/AuthProvider.kt
+++ sshserver/src/main/kotlin/net/adamcin/httpsig/jce/AuthProvider.kt
@@ -1,9 +1,8 @@
-package it.admiral0.technicantani.ssh
+package net.adamcin.httpsig.jce
 
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
 import com.google.common.cache.LoadingCache
-import io.vertx.core
 import io.vertx.core.AsyncResult
 import io.vertx.core.Handler
 import io.vertx.core.Vertx
@@ -12,24 +11,22 @@ import io.vertx.ext.auth.AbstractUser
 import io.vertx.ext.auth.AuthProvider
 import io.vertx.ext.auth.User
 import it.admiral0.technicantani.ssh
-import net.adamcin.httpsig.api.Keychain
+import net.adamcin.httpsig.api.Base64
 import net.adamcin.httpsig.jce.AuthorizedKeys
 import org.apache.sshd.server.PublickeyAuthenticator
 import org.apache.sshd.server.session.ServerSession
 import java.io.File
 import java.security.PublicKey
-import java.util.*
-import java.util.regex.Pattern
 import kotlin.properties.Delegates
 import kotlin.text.Regex
 
 class AuthException() : Exception()
 
 
-class User(val username : String, val permissions: List<String>) : AbstractUser() {
+class AntaniUser(val username : String, val permissions: List<String>) : AbstractUser() {
     var auth : AuthProvider by Delegates.notNull()
     override fun doIsPermitted(permission: String, resultHandler: Handler<AsyncResult<Boolean>>) {
-        resultHandler.handle(object : AsyncResult<Boolean>{
+        resultHandler.handle(object : AsyncResult<Boolean> {
             override fun cause(): Throwable? {
                 return null
             }
@@ -40,7 +37,7 @@ class User(val username : String, val permissions: List<String>) : AbstractUser(
                 return true
             }
             override fun result(): Boolean? {
-                return permission in this@User.permissions
+                return permission in this@AntaniUser.permissions
             }
         })
     }
@@ -56,48 +53,39 @@ class User(val username : String, val permissions: List<String>) : AbstractUser(
     }
 }
 
-class PubkeyAuthProvider(val vertx: Vertx) : AuthProvider, PublickeyAuthenticator {
-    var userCache : LoadingCache<String, ssh.User> = CacheBuilder.newBuilder().maximumSize(15).build(object : CacheLoader<String,ssh.User>(){
-        override fun load(key: String): ssh.User? {
+public class PubkeyAuthProvider(val vertx: Vertx) : AuthProvider, PublickeyAuthenticator {
+    var userCache : LoadingCache<String, AntaniUser> = CacheBuilder.newBuilder().maximumSize(10).build(object : CacheLoader<String, it.admiral0.technicantani.ssh.User>(){
+        override fun load(key: String): AntaniUser {
             val path = vertx.getOrCreateContext().config().getString("authStore")
             val perms = File(path + File.separator + key)
             if(!perms.exists() || !perms.canRead() )
                 throw AuthException()
-            return ssh.User(
-                        key,
-                        perms.readLines(Charsets.UTF_8)
-                    )
+            return AntaniUser(
+                    key,
+                    perms.readLines(Charsets.UTF_8)
+            )
         }
     })
-    val authorizedKeys : List<Triple<String,String,String>> by Delegates.lazy {
+    val authorizedKeys : List<AuthorizedKeys.AuthorizedKey> by Delegates.lazy {
         val path = vertx.getOrCreateContext().config().getString("pubKeyStore")
         val pattern = Regex("^([^\\s]+)\\s+([^\\s]+)(\\s+([^\\s].*)|$)")
         return@lazy File(path).readLines(Charsets.UTF_8).filter { it.matches(pattern) }
                 .map {
-                    val groups = pattern.match(it).groups
-                    return@map Triple(
-                            groups.get(1)?.value ?: "" ,
-                            groups.get(2)?.value ?: "",
-                            groups.get(3)?.value ?: ""
-                            )
+                    return@map AuthorizedKeys.parseAuthorizedKey(it)
                 }
     }
-    var authKeyCache : LoadingCache<PublicKey, String?> = CacheBuilder.newBuilder().maximumSize(50).build(object : CacheLoader<PublicKey,String?>(){
+    var authKeyCache : LoadingCache<PublicKey, String?> = CacheBuilder.newBuilder().maximumSize(50).build(object : CacheLoader<PublicKey, String?>(){
         override fun load(key: PublicKey): String? {
-            for(key in authorizedKeys){
-
+            for(auth in authorizedKeys){
+                if(key.getEncoded() == auth.getEncodedKey().toByteArray(Charsets.UTF_8)){
+                    return auth.getComment().splitBy("|", limit = 2)[0]
+                }
             }
+            return null
         }
     })
-    var pubkeyCache : Map<PublicKey,String?> by Delegates.lazy {
-
-
-    }
-    init {
-
-    }
     override fun authenticate(authInfo: JsonObject, resultHandler: Handler<AsyncResult<User>>) {
-        throw UnsupportedOperationException()
+        val user = authInfo.getString("user")
     }
 
     override fun authenticate(username: String, key: PublicKey, session: ServerSession): Boolean {
